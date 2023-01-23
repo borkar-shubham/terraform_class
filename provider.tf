3c060e12f9cdba8d5e771da85930304b1992b6c1
Shubham Borkar <shubham_borkar@outlook.com>
2023-01-23 20:06:43 +0530

    Added EC2 instance

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = shubham
+  region = "eu-north-1"
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "ami" {
+  type = string
+  default = "ami-061eda1ede961cfb2"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t3.micro"
+}
+
+variable "security_groups" {
+  type = string
+  default = "sg-03bf5ae22b55426ea"
+}
